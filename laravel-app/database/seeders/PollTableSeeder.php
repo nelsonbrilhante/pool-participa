a699507b94dc71eb879d669612b925b59d2c458c
Nelson Brilhante <nelsonbrilhante@gmail.com>
2023-09-12 12:51:16 +0100

    Poll App v0.94
    Changes:
    - Added full content to poll seeders;
    - Added modal loading screen while importing new voters;

--- laravel-app/database/seeders/PollTableSeeder.php
+++ laravel-app/database/seeders/PollTableSeeder.php
@@ -16,7 +16,7 @@ class PollTableSeeder extends Seeder
     {
         Poll::create([
             'title' => 'Orçamento Participativo 2023',
-            'description' => 'Em traços largos, o OP assume-se como um instrumento relevante, com grandes potencialidades mas também limites, que contribui para a construção/consolidação da cidadania e da democracia ao nível local.',
+            'description' => 'O Orçamento Participativo do Município de Nazaré (Orçamento Participativo) é um projeto da Câmara Municipal que tem como primordial objetivo a colheita de contributos de todos os cidadãos que pretendam ter papel ativo na implementação e execução de ações que vão encontro das mais basilares carências sociais. São premissas fundamentais desta medida solidificar o vínculo entre a autarquia e os seus munícipes e, consequentemente, aprofundar a qualidade do processo democrático local, pois os orçamentos participativos exprimem o enraizamento da participação democrática e da ligação dos cidadãos à causa pública, sem prejuízo dos contributos que sempre foram consagrados, nomeadamente, dos Partidos Políticos (no âmbito do respeito pelo Estatuto do Direito à Oposição), dos Órgãos das Freguesias e da recolha sistemática de sugestões dos Munícipes e outras entidades, materializada na audição contínua dos cidadãos, das coletividades, das entidades públicas e privadas e de outros canais de comunicação com o Executivo, colocados diretamente à disposição de toda e qualquer pessoa.',
             'status' => 'active',
         ]);
     }
--- laravel-app/resources/views/voter/import.blade.php
+++ laravel-app/resources/views/voter/import.blade.php
@@ -29,7 +29,8 @@
         <!-- Form -->
         <div class="row mt-3">
             <div class="col-12">
-                <form action="{{ route('import-voters') }}" method="POST" enctype="multipart/form-data">
+                <form action="{{ route('import-voters') }}" method="POST" enctype="multipart/form-data"
+                    id="csv-import-form">
                     @csrf
                     <div class="mb-3">
                         <label for="csv_file" class="form-label">Carregar ficheiro CSV</label>
@@ -39,5 +40,58 @@
                 </form>
             </div>
         </div>
+
+        <!-- Loading Modal -->
+        <div class="modal fade" id="loadingModal" tabindex="-1" aria-labelledby="loadingModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="loadingModalLabel">A importar eleitores...</h5>
+                    </div>
+                    <div class="modal-body">
+                        <div class="d-flex justify-content-center">
+                            <div class="spinner-border" role="status">
+                                <span class="visually-hidden">Carregando...</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script>
+            document.getElementById('csv-import-form').addEventListener('submit', function(e) {
+                e.preventDefault();
+
+                // Create a new instance of the modal
+                var loadingModal = new bootstrap.Modal(document.getElementById('loadingModal'));
+                // Show the loading modal
+                loadingModal.show();
+
+                // Submit the form using Fetch API
+                fetch(this.action, {
+                        method: 'POST',
+                        body: new FormData(this)
+                    })
+                    .then(response => {
+                        if (response.redirected) {
+                            window.location.href = response.url; // Redirect the browser to the new URL
+                            return;
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        if (data) {
+                            loadingModal.hide();
+                            alert(data.message);
+                        }
+                    })
+                    .catch(error => {
+                        loadingModal.hide();
+                        alert('Ocorreu um erro ao importar o arquivo CSV.');
+                    });
+            });
+        </script>
+
     </div>
 @endsection
